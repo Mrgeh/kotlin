0224f4ef99642890fd53f5d8d0acf40caefc8c78
gehang <zyxxjsgehang1108>
2020-11-20 15:00:18 +0800

    多个初始块,

---
+++ app/src/main/java/com/xtjc/kotlinstudy/activity/base/Basebean.kt
@@ -0,0 +1,11 @@
+package com.xtjc.kotlinstudy.activity.base
+
+/**
+ *
+ * @packageName com.xtjc.kotlinstudy.activity.base
+ * @author Ge Hang
+ * @Admonish   This is the ancestral code from Ge Hang, please check!
+ * @time 2020/11/20 15:00
+ */
+class Basebean {
+}
